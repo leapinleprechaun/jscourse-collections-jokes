ba6d07aee62abc00c468911da7013eca1472c1df
Gill <leapin_leprechaun@hotmail.com>
2018-02-15 20:30:02 +0000

    Added more explicit comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,28 @@
-// Model
+// Model - create a model constructor
 var Joke = Backbone.Model.extend({
 });
 
-// Collection
+// Collection - create a collection constructor
 var Jokes = Backbone.Collection.extend({
 	url: 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten',
 	model: Joke
 });
 
-
+// create a new Instance of the Jokes collection
 var jokesList =  new Jokes();
 
+// Send a request to the url to get the jokes
 jokesList.fetch().then(function(response){
-	_.each(jokesList.where({type: "knock-knock"}), function(joke){
+	//response argument gives you all of the returned information here if you want to debugger and check it here
+
+	// Get a copy of only the knock knock jokes by filtering on the type attribute
+	var justKnockKnocks = jokesList.where({type: "knock-knock"});
+
+	// loop through the knock knock jokes after it has been fetched
+	_.each(justKnockKnocks, function(joke){
+		//The joke argument here holds one joke model
+
+		// use the .get method to get information from that model
 		console.log(joke.get('setup'));
 		console.log(joke.get('punchline'));
 	});
